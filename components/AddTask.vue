0499144075333a1c152cebe27708af177ed42d19
Łukasz Fuchs <lukaszfuchs18@gmail.com>
2020-09-09 23:26:53 +0200

    config project and add baseTask, addTask components

---
+++ components/AddTask.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="columns">
+        <form @submit="addTask" class="medium-1">
+            <input
+                @value="title"
+                type="text"
+                placeholder="Title"
+            >
+            <input
+                @value="description"
+                type="text"
+                placeholder="Description"
+            >
+            <input
+                type="submit"
+                value="Add"
+            >
+        </form>
+    </div>
+</template>
+
+<script>
+import BaseTask from '~/components/BaseTask.vue'
+import AddTask from '~/components/AddTask.vue'
+export default {
+  name: 'IndexPage',
+  data () {
+    return {
+      title: '',
+      description: '',
+    }
+  },
+  components: {
+
+  },
+  methods: {
+    addTask: function (){
+      console.log("siema");
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="row container">
+    <div class="columns">
+      <AddTask />
+    </div>
+    <main class="columns small-12">
+
+      
+
+    </main>
+  </div>
+</template>
+
+<script>
+import BaseTask from '~/components/BaseTask.vue'
+import AddTask from '~/components/AddTask.vue'
+export default {
+  name: 'IndexPage',
+  data () {
+    return {
+      messege: '',
+    }
+  },
+  components: {
+
+  },
+  methods: {
+    addTask: function (){
+      console.log("siema");
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+</style>
